e0ccff5b45dba62a8ff872891a5de3616532b9f2
Chris <chrismjboyle@gmail.com>
2018-07-24 02:15:38 +0100

    Updated

--- src/pfs/nodes.py
+++ src/pfs/nodes.py
@@ -1,7 +1,1 @@
-class ProcessNode(object):
-    
-    def __init__(self, type, key, reactions=list(), conversions=list()):
-        self.type           = type
-        self.key            = key
-        self.reactions      = reactions
-        self.conversions    = conversions
+
