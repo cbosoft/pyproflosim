7540a0b65bd112861ef87f8040108cf5fcb44ad8
Chris <chrismjboyle@gmail.com>
2018-07-25 01:49:45 +0100

    More verbose startup

--- src/pfs/main.py
+++ src/pfs/main.py
@@ -7,8 +7,10 @@ from pfs.vis import draw
 from pfs.chemical_component import ChemicalComponent
 
 def main():
+    print("Creating process...")
     process = Process()
-    
+
+    print("Adding nodes...")
     ### Nodes ###
     process.add_node("air_inlet", NodeType.INPUT, UnitType.INLET)
     process.add_node("methanol_inlet", NodeType.INPUT, UnitType.INLET)
@@ -23,6 +25,7 @@ def main():
     process.add_node("offgas_outlet", NodeType.OUTPUT, UnitType.OUTLET)
     process.add_node("product_outlet", NodeType.OUTPUT, UnitType.OUTLET)
 
+    print("Adding streams...")
     ### Streams ###
     process.add_stream("methanol_inlet", process.nodes["methanol_inlet"], process.nodes["mix_9_1"])
     process.add_stream("air_inlet", process.nodes["air_inlet"], process.nodes["mix_2_3"])
@@ -47,12 +50,17 @@ def main():
     process.fill_nodes()
 
     #print(process)
-    draw(process, "process.pdf")
+    #draw(process, "process.pdf")
     #print(process.graph.number_of_selfloops())
 
     #process.get_stream("product_outlet").add_component(ChemicalComponent("methanol"), 0.9866)
     #process.get_stream("product_outlet").add_component(ChemicalComponent("formaldehyde"), 2.775)
     #process.get_stream("product_outlet").add_component(ChemicalComponent("water"), 3.7386)
     
-    for s in process.get_streams():
-        print(s)
+    #for s in process.get_streams():
+    #    print(s)
+
+    #print(process.streams['water_inlet'])
+    print("Taking mass balance over a101")
+    expr, sol, inp, out = process.nodal_mass_balance(process.nodes['a101'])
+    print(expr)
