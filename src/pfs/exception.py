daf5412a866d20a9ac6d5fe02c939598319f0ec1
Chris <chrismjboyle@gmail.com>
2018-07-30 13:17:34 +0100

    Added exceptional code

--- src/pfs/exception.py
+++ src/pfs/exception.py
@@ -4,3 +4,7 @@ class PFS_Error(Exception):
 
 class PFS_Key_Duplication_Error(PFS_Error):
     '''Duplicate keys will be generated (try using a different name).'''
+
+    
+class PFS_Component_Not_Found_Error(PFS_Error):
+    '''Could not find the specified compound in the PubChem database'''
