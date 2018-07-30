dc394dae0bde56a558dc8145cb3369ff05b0d014
Chris <chrismjboyle@gmail.com>
2018-07-30 13:17:50 +0100

    Added constructor for chemical component

--- src/pfs/chemical_component.py
+++ src/pfs/chemical_component.py
@@ -1,7 +1,13 @@
-import pubchempy
-
+import pubchempy as pcp
+from pfs.exception import PFS_Component_Not_Found_Error
 
 class ChemicalComponent(object):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, idx=0):
+        res = pcp.get_compounds(name, 'name')
+        if not len(res):
+            raise PFS_Component_Not_Found_Error(f'No compounds with name \'{name}\' found!')
+        res = res[idx]
+        self.name = res.iupac_name
+        self.molecular_weight = res.exact_mass
+        self.data = res.to_dict()
