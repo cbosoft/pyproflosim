eb74cc9d9a4fb77e13f66bb1185657bd3a64656e
Chris <chrismjboyle@gmail.com>
2019-10-18 11:05:21 +0100

    Created a generic unit object
    Returns output given a transfer function (defined in sub classes) and
    the inputs (presumed to be other units).

--- pfs/units.py
+++ pfs/units.py
@@ -1,4 +1,18 @@
-class UnitType:
-    INLET = 0
-    OUTLET = 1
+class GenericUnit:
+    '''Generic Process Unit class'''
 
+    def __init__(self, inputs, outputs):
+
+        if not isinstance(inputs, list):
+            self.inputs = [inputs]
+
+        if not isinstance(outputs, list):
+            self.outputs = [outputs]
+    
+    def get_transfer_function(self):
+        raise NotImplementedError('Feature not implemented in generic class; use sub class.')
+
+    def get_output(self, objdir):
+        tf = self.get_transfer_function()
+        input_objects = [objdir[inp] for inp in self.inputs]
+        output = tf(*[iobj.get_output() for iobj in input_objects])
